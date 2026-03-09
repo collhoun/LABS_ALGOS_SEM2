a2996f759724b6abb8c11e159f66369a0e784e22
collhoun <2904yr@mail.ru>
2026-03-09 17:39:59 +0300

    Add main functions in lab2

---
+++ Lab2/src/data_creators/create_samples.py
@@ -0,0 +1,91 @@
+
+def create_samples(n: int) -> list:
+    """
+    создает n пар ключ-значение  
+
+    Args:
+        n (int): количество пар
+
+    Returns:
+        list: список пар
+    """
+    possible_keys = ['key_' + str(i) for i in range(n)]
+    possible_values = ['value_' + str(i) for i in range(n)]
+    return list(zip(possible_keys, possible_values))
+
+
+def write_samples(samples: list, file_name: str) -> bool:
+    """
+    записывает примеры в файл
+
+    Args:
+        samples (list): входные данные
+        file_name (str): имя файля для записи
+
+    Returns:
+        bool: True если запись прошла, False в противном случае
+    """
+    try:
+        with open(file_name, 'w') as f:
+            f.write(str(len(samples)) + '\n')
+            for key, value in samples:
+                f.write(key + ' ' + value + '\n')
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+
+def create_sorted_samples(n: int) -> list:
+    """
+    создает n пар ключ-значение и сортирует их
+
+    Args:
+        n (int): количетсво пар
+
+    Returns:
+        list: список отсортированных пар
+    """
+    samples = create_samples(n)
+    samples.sort()
+    return samples
+
+
+def create_reversed_samples(n: int) -> list:
+    """
+    создает n пар ключ-значение и переворачивает их
+
+    Args:
+        n (int): количетсво пар
+
+    Returns:
+        list: список перевернутых пар
+    """
+    samples = create_samples(n)
+    samples.reverse()
+    return samples
+
+
+def create_random_samples(n: int) -> list:
+    """
+    создает n пар ключ-значение и перемешивает их
+
+    Args:
+        n (int): количетсво пар
+
+    Returns:
+        list: перемешанный список
+    """
+    import random
+    samples = create_samples(n)
+    random.shuffle(samples)
+    return samples
+
+
+if __name__ == '__main__':
+    sorted_samples = create_sorted_samples(20)
+    reversed_samples = create_reversed_samples(20)
+    random_samples = create_random_samples(20)
+    write_samples(sorted_samples, './data_samples/sorted_samples.txt')
+    write_samples(reversed_samples, './data_samples/reversed_samples.txt')
+    write_samples(random_samples, './data_samples/random_samples.txt')
---
+++ Lab2/src/some.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct MyStructure
+{
+    const char *key;
+    const char *value;
+} MyStructure;
+
+int read_samples(const char *filename, MyStructure **samples)
+{
+    FILE *file = fopen(filename, "r");
+    if (!file)
+    {
+        return -1;
+    }
+
+    int len_samples;
+    if (fscanf(file, "%d", &len_samples) != 1)
+    {
+        fclose(file);
+        return -2;
+    }
+
+    *samples = (MyStructure *)malloc(len_samples * sizeof(MyStructure));
+    if (*samples == NULL)
+    {
+        fclose(file);
+        return -3;
+    }
+
+    char key_buffer[1024];
+    char value_buffer[1024];
+
+    for (int i = 0; i < len_samples; i++)
+    {
+        if (fscanf(file, "%s %s", key_buffer, value_buffer) != 2)
+        {
+            // нужна ли здесь проверка на память?
+            free(*samples);
+            fclose(file);
+            return -4;
+        }
+
+        (*samples)[i].key = strdup(key_buffer);
+        (*samples)[i].value = strdup(value_buffer);
+    }
+
+    fclose(file);
+    return len_samples;
+}
+
+int binary_search_insert(MyStructure *samples, int len, const char *key)
+{
+    int left = 0, right = len;
+    while (left < right)
+    {
+        int mid = (left + right) / 2;
+        if (strcmp(samples[mid].key, key) < 0)
+            left = mid + 1;
+        else
+            right = mid;
+    }
+    return left;
+}
+int insertion_sort(MyStructure *samples, int len)
+{
+    for (int i = 1; i < len; i++)
+    {
+        MyStructure tmp = samples[i];
+        int pos = binary_search_insert(samples, i, tmp.key);
+
+        for (int j = i; j > pos; j--)
+        {
+            samples[j] = samples[j - 1];
+        }
+        samples[pos] = tmp;
+    }
+    return 1;
+}
+
+void demonstration(const char *filename)
+{
+    MyStructure *samples;
+    int len_samples = read_samples(filename, &samples);
+    printf("------------------------------\n");
+    printf("Before sort:\n");
+    for (int i = 0; i < len_samples; i++)
+    {
+        printf("%s %s\n", samples[i].key, samples[i].value);
+    }
+    insertion_sort(samples, len_samples);
+    printf("------------------------------\n");
+    printf("After sort:\n");
+    for (int i = 0; i < len_samples; i++)
+    {
+        printf("%s %s\n", samples[i].key, samples[i].value);
+    }
+
+    const char *key1 = malloc(1024);
+    const char *key2 = malloc(1024);
+    const char *key3 = malloc(1024);
+    if (!key1 || !key2 || !key3)
+    {
+        fprintf(stderr, "Ошибка выделения памяти\n");
+        return;
+    }
+    printf("Enter three keys to find:\n");
+    scanf("%s %s %s", key1, key2, key3);
+
+    int pos1 = binary_search_insert(samples, len_samples, key1);
+    int pos2 = binary_search_insert(samples, len_samples, key2);
+    int pos3 = binary_search_insert(samples, len_samples, key3);
+    if (pos1 >= 0)
+    {
+        printf("%s %s\n", samples[pos1].key, samples[pos1].value);
+    }
+    if (pos2 >= 0)
+    {
+        printf("%s %s\n", samples[pos2].key, samples[pos2].value);
+    }
+    if (pos3 >= 0)
+    {
+        printf("%s %s\n", samples[pos3].key, samples[pos3].value);
+    }
+
+    free(samples);
+    free((void *)key1);
+    free((void *)key2);
+    free((void *)key3);
+}
+
+int main()
+{
+    demonstration("data_samples/random_samples.txt");
+    demonstration("data_samples/sorted_samples.txt");
+    demonstration("data_samples/reversed_samples.txt");
+    return 0;
+}
