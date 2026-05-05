2b25b5787fb796ba99f85442908625618f75266a
collhoun <2904yr@mail.ru>
2026-05-05 17:03:56 +0300

    Add interactive mode

--- Lab5/src/main.c
+++ Lab5/src/main.c
@@ -4,9 +4,11 @@
 int main()
 {
     FILE *fp = NULL;
+    printf("Graph Analysis Program\n");
+    printf("This program analyzes a graph for cycles and computes shortest paths if acyclic.\n\n");
     printf("Choose input method:\n");
-    printf("1. Console input\n");
-    printf("2. File input\n");
+    printf("1. Console input (manual entry)\n");
+    printf("2. File input (read from file)\n");
     printf("Enter choice (1 or 2): ");
     int choice;
     scanf("%d", &choice);
@@ -18,20 +20,29 @@ int main()
         fp = fopen(filename, "r");
         if (!fp)
         {
-            printf("Error opening file %s\n", filename);
+            printf("Error: Could not open file '%s'. Please check the filename and try again.\n", filename);
             return 1;
         }
+        printf("Reading graph data from file '%s'...\n", filename);
     }
     else
+    {
         fp = stdin;
+        printf("Entering data manually with console.\n");
+    }
 
     int size_edges, size_vertices;
+    printf("Enter the number of edges and vertices (format: edges vertices): ");
     fscanf(fp, "%d %d", &size_edges, &size_vertices);
+    printf("Graph has %d edges and %d vertices.\n", size_edges, size_vertices);
     Edge *edges = create_array(size_edges);
     for (int i = 0; i < size_edges; i++)
     {
+        if (fp == stdin)
+            printf("Enter edge %d (source destination weight): ", i + 1);
         fscanf(fp, "%d %d %d", &edges[i].src, &edges[i].dest, &edges[i].weight);
     }
+    printf("All edges read successfully.\n");
 
     if (fp != stdin)
         fclose(fp);
